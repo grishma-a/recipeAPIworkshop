a84558ded176794650f56d77296b36fa8cbec11a
grishma-a <108956544+grishma-a@users.noreply.github.com>
2026-02-19 14:33:14 -0600

    redid

--- script.js
+++ script.js
@@ -1,1 +1,74 @@
-alert('Welcome to my website!');
+const searchBox = document.querySelector('.searchBox');
+const searchBtn = document.querySelector('.searchBtn');
+const recipeContainer = document.querySelector('.recipe-container');
+
+const recipeDetailsContent = document.querySelector('.recipe-details-content');
+const recipeCloseBtn = document.querySelector('.recipe-close-btn');
+
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('Search button clicked');
+
+
+    const searchInput = searchBox.value.trim();
+    fetchRecipes(searchInput);
+});
+
+const fetchRecipes = async (query) => {
+    const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    const response = await data.json();
+    console.log(response);
+    recipeContainer.innerHTML = ''; // Clear previous results
+
+    //now we have them in the log, we want to start displaying them on the page
+    response.meals.forEach(meal => {
+        const recipeDiv = document.createElement('div');
+        recipeDiv.classList.add('recipe');
+
+        recipeDiv.innerHTML = `
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+            <h2>${meal.strMeal}</h2>
+            <p>${meal.strCategory}</p>
+        `
+
+        const button = document.createElement('button');
+        button.textContent = 'view recipe';
+        recipeDiv.appendChild(button);
+
+        button.addEventListener('click', () => {
+            openRecipePopup(meal);
+        });
+
+        recipeContainer.appendChild(recipeDiv);
+    });
+}
+
+const openRecipePopup = (meal) => {
+    recipeDetailsContent.innerHTML = `
+        <h2>${meal.strMeal}</h2>
+        <h3>Ingredients:</h3>
+        <ul> ${getIngredientsList(meal)}</ul>
+        <h3>Instructions:</h3>
+        <p>${meal.strInstructions}</p>
+    `
+    recipeDetailsContent.parentElement.style.display = 'block';
+}
+
+const getIngredientsList = (meal) => {
+    let ingredientsList = '';
+    for (let i = 1; i <= 20; i++) {
+        const ingredient = meal[`strIngredient${i}`];
+        const measure = meal[`strMeasure${i}`];
+        if (ingredient) {
+            ingredientsList += `<li>${measure} ${ingredient}</li>`;
+        }
+        else {
+            break;
+        }
+    }
+    return ingredientsList;
+}
+
+recipeCloseBtn.addEventListener('click', () => {
+    recipeDetailsContent.parentElement.style.display = 'none';
+});
