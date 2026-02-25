34b5c8d60c66fef989883579ea4b47f89e5f89cd
grishma-a <108956544+grishma-a@users.noreply.github.com>
2026-02-25 14:07:54 -0600

    demo for wics

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-alert('Welcome to my website!');
+const searchBox = document.querySelector('.searchBox');
+const searchBtn = document.querySelector('.searchBtn');
+const recipeContainer = document.querySelector('.recipe-container');
+
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault(); // Prevent the form from submitting
+    console.log("clicked button");
+
+    const searchInput = searchBox.value.trim();
+    fetchRecipes(searchInput);
+})
+
+const fetchRecipes = async (query) => {
+    const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    const response = await data.json();
+    console.log(response);
+
+
+    response.meals.forEach(meal => {
+        const recipeDiv = document.createElement('div');
+        recipeDiv.classList.add('recipe');
+        recipeDiv.innerHTML = `
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+            <h2>${meal.strMeal}</h2>
+            <p>${meal.strCategory}</p>
+        `;
+        recipeContainer.appendChild(recipeDiv);
+    });
+}
